14b532ded4200b7a87fc0df765868999a4662f83
Eugene <100863231+Gaidysheff@users.noreply.github.com>
2022-05-18 15:24:05 +0500

    Update urls.py

--- News_Project/News_Project/urls.py
+++ News_Project/News_Project/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('news/', include('newsapp.urls')),
+    path('news/search/', include('newsapp.urls')),
     path('articles/', include('newsapp.urls_for_articles')),
 ]
